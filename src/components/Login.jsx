2f61c33ccb9735f0a91c315c0ec790c7b65ddcea
Fahad <136419272+faddy1184@users.noreply.github.com>
2025-07-12 13:08:06 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = ({ setIsAuthenticated }) => {
     setError(null);
 
     try {
-      const response = await fetch("https://3dportfolio-lime.vercel.app/login", {
+      const response = await fetch("https://backend-one-sable.vercel.app/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
